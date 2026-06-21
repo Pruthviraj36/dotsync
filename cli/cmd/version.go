2822ff690bdd6ce91b8dcd351c92fd3cb50ad402
Pruthviraj36 <chauhanpruthviraj309@gmail.com>
2026-06-21 10:39:08 +0530

    feat: added a new command for upadating

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -2,30 +2,46 @@ package cmd
 
 import (
 	"fmt"
+	"runtime"
 	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
+
+var Version = "dev" // Can be overridden by ldflags
 
 func versionCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version number of DotSync",
 		Run: func(cmd *cobra.Command, args []string) {
-			version := "unknown"
+			version := Version
+			revision := "unknown"
+			buildTime := "unknown"
+
 			if info, ok := debug.ReadBuildInfo(); ok {
+				if version == "dev" && info.Main.Version != "" && info.Main.Version != "(devel)" {
+					version = info.Main.Version
+				}
 				for _, setting := range info.Settings {
 					if setting.Key == "vcs.revision" {
-						// take first 7 chars of commit hash
-						if len(setting.Value) > 7 {
-							version = setting.Value[:7]
-						} else {
-							version = setting.Value
+						revision = setting.Value
+						if len(revision) > 7 {
+							revision = revision[:7]
 						}
+					}
+					if setting.Key == "vcs.time" {
+						buildTime = setting.Value
 					}
 				}
 			}
-			fmt.Printf("DotSync version: %s\n", version)
+
+			fmt.Println("DotSync CLI")
+			fmt.Printf("  Version:    %s\n", version)
+			fmt.Printf("  Revision:   %s\n", revision)
+			fmt.Printf("  Build Time: %s\n", buildTime)
+			fmt.Printf("  OS/Arch:    %s/%s\n", runtime.GOOS, runtime.GOARCH)
+			fmt.Printf("  Go Version: %s\n", runtime.Version())
 		},
 	}
 }
